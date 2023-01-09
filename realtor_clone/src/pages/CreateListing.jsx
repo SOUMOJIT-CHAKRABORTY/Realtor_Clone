f69f8787fac7b469b8b0b2d3b0ed9edd1ab61c4c
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2023-01-09 23:41:36 +0530

    form UI is created

--- realtor_clone/src/pages/CreateListing.jsx
+++ realtor_clone/src/pages/CreateListing.jsx
@@ -4,15 +4,37 @@ import { useState } from "react";
 export default function CreateListing() {
   const [formData, setFormData] = useState({
     type: "rent",
+    name: "",
+    bedrooms: 1,
+    bathrooms: 1,
+    parking: false,
+    furnished: false,
+    address: "",
+    description: "",
+    offer: false,
+    regularPrice: "",
+    discountedPrice: "",
   });
-  const { type } = formData;
+  const {
+    type,
+    name,
+    bedrooms,
+    bathrooms,
+    parking,
+    furnished,
+    address,
+    description,
+    offer,
+    regularPrice,
+    discountedPrice,
+  } = formData;
   function onChange() {}
   return (
     <main className="mx-auto px-2 max-w-md">
       <h1 className="text-3xl font-bold mt-6">Create a Listing</h1>
       <form className="">
         <p className="text-left mt-6 font-semibold text-lg">Sell/Rent</p>
-        <div className="flex">
+        <div className="flex mb-6">
           <button
             id="type"
             value="sale"
@@ -38,6 +60,208 @@ export default function CreateListing() {
             rent
           </button>
         </div>
+        <section className="text-left mb-6">
+          <p className="mt-6 font-semibold text-lg">Name</p>
+          <input
+            type="text"
+            id="name"
+            placeholder="Name"
+            maxLength="32"
+            minLength="10"
+            required
+            value={name}
+            onChange={onChange}
+            className="w-full text-xl border border-gray-300 bg-white rounded text:sm text-gray-700 focus:bg-white focus:text-gray-700 focus:border-slate-600"
+          ></input>
+        </section>
+        <div className="flex mb-6 space-x-6">
+          <div className="text-left">
+            <p className="font-semibold text-lg">Beds</p>
+            <section className="">
+              <input
+                type="number"
+                id="bedrooms"
+                value={bedrooms}
+                onChang={onChange}
+                min="1"
+                max="50"
+                required
+                className="w-full text-gray-700 border border-gray-300 text-xl rounded focus:bg-white px-4 py-2 bg-white focus:border-slate-600 text-center"
+              ></input>
+            </section>
+          </div>
+          <div className="text-left">
+            <p className="font-semibold text-lg">Baths</p>
+            <section className="">
+              <input
+                type="number"
+                id="bedrooms"
+                value={bathrooms}
+                onChang={onChange}
+                min="1"
+                max="50"
+                required
+                className="w-full text-gray-700 border border-gray-300 text-xl rounded focus:bg-white px-4 py-2 bg-white focus:border-slate-600 text-center"
+              ></input>
+            </section>
+          </div>
+        </div>
+        <p className="text-left mt-6 font-semibold text-lg">Parking Spot</p>
+        <div className="flex mb-6">
+          <button
+            id="parking"
+            value={true}
+            onClick={onChange}
+            className={`w-full mr-3 shadow-md text-sm uppercase rounded hover:shadow-lg p-2 focus:shadow-lg active:shadow-lg transition ease-in-out duration-200 font-medium ${
+              !parking ? "bg-white text-black" : "bg-slate-600 text-white"
+            }`}
+          >
+            yes
+          </button>
+          <button
+            id="parking"
+            value={false}
+            onClick={onChange}
+            className={`w-full ml-3 shadow-md text-sm uppercase rounded hover:shadow-lg p-2 focus:shadow-lg active:shadow-lg transition ease-in-out duration-200 font-medium ${
+              parking ? "bg-white text-black" : "bg-slate-600 text-white"
+            }`}
+          >
+            no
+          </button>
+        </div>
+        <p className="text-left mt-6 font-semibold text-lg">Furnished</p>
+        <div className="flex mb-6">
+          <button
+            id="furnished"
+            value={true}
+            onClick={onChange}
+            className={`w-full mr-3 shadow-md text-sm uppercase rounded hover:shadow-lg p-2 focus:shadow-lg active:shadow-lg transition ease-in-out duration-200 font-medium ${
+              !furnished ? "bg-white text-black" : "bg-slate-600 text-white"
+            }`}
+          >
+            yes
+          </button>
+          <button
+            id="furnished"
+            value={false}
+            onClick={onChange}
+            className={`w-full ml-3 shadow-md text-sm uppercase rounded hover:shadow-lg p-2 focus:shadow-lg active:shadow-lg transition ease-in-out duration-200 font-medium ${
+              furnished ? "bg-white text-black" : "bg-slate-600 text-white"
+            }`}
+          >
+            no
+          </button>
+        </div>
+        <section className="text-left mb-6">
+          <p className="mt-6 font-semibold text-lg">Address</p>
+          <textarea
+            type="text"
+            id="address"
+            placeholder="Address"
+            required
+            value={address}
+            onChange={onChange}
+            className="w-full text-xl border border-gray-300 bg-white rounded text:sm text-gray-700 focus:bg-white focus:text-gray-700 focus:border-slate-600"
+          ></textarea>
+        </section>
+        <section className="text-left mb-6">
+          <p className="font-semibold text-lg">Description</p>
+          <textarea
+            type="text"
+            id="description"
+            placeholder="Description"
+            required
+            value={description}
+            onChange={onChange}
+            className="w-full text-xl border border-gray-300 bg-white rounded text:sm text-gray-700 focus:bg-white focus:text-gray-700 focus:border-slate-600"
+          ></textarea>
+        </section>
+        <p className="text-left  font-semibold text-lg">Offers</p>
+        <div className="flex mb-6">
+          <button
+            id="offer"
+            value={true}
+            onClick={onChange}
+            className={`w-full mr-3 shadow-md text-sm uppercase rounded hover:shadow-lg p-2 focus:shadow-lg active:shadow-lg transition ease-in-out duration-200 font-medium ${
+              !offer ? "bg-white text-black" : "bg-slate-600 text-white"
+            }`}
+          >
+            yes
+          </button>
+          <button
+            id="offer"
+            value={false}
+            onClick={onChange}
+            className={`w-full ml-3 shadow-md text-sm uppercase rounded hover:shadow-lg p-2 focus:shadow-lg active:shadow-lg transition ease-in-out duration-200 font-medium ${
+              offer ? "bg-white text-black" : "bg-slate-600 text-white"
+            }`}
+          >
+            no
+          </button>
+        </div>
+        <div className="text-left mb-6">
+          <p className="font-semibold text-lg">Regular Price</p>
+          <div className="flex items-center space-x-6">
+            <div className="">
+              <input
+                type="number"
+                id="regularPrice"
+                value={regularPrice}
+                onChang={onChange}
+                min="100"
+                max="50000000"
+                required
+                className="w-full text-gray-700 border border-gray-300 text-xl rounded focus:bg-white px-4 py-2 bg-white focus:border-slate-600 text-center"
+              ></input>
+            </div>
+            {type === "rent" && (
+              <div>
+                <p className="whitespace-nowrap text-md w-full">$ / Month</p>
+              </div>
+            )}
+          </div>
+        </div>
+        {offer && (
+          <div className="text-left mb-6">
+            <p className="font-semibold text-lg">Discounted Price</p>
+            <div className="flex items-center space-x-6">
+              <div className="">
+                <input
+                  type="number"
+                  id="discountedPrice"
+                  value={discountedPrice}
+                  onChang={onChange}
+                  min="100"
+                  max="50000000"
+                  required={offer}
+                  className="w-full text-gray-700 border border-gray-300 text-xl rounded focus:bg-white px-4 py-2 bg-white focus:border-slate-600 text-center"
+                ></input>
+              </div>
+            </div>
+          </div>
+        )}
+        <div className="text-left mb-6">
+          <p className="font-semibold text-xl">Images</p>
+          <p className="text-gray-600">
+            The first image will be the cover (max 6)
+          </p>
+          <input
+            type="file"
+            id="images"
+            onChange={onChange}
+            accept=".jpg , .png, .jpeg"
+            multiple
+            required
+            className="w-full bg-white px-3 py-1.5 text-gray-700 border-gray-300-rounded transition duration-150 ease-in-out focus:bg-white focus:border-slate-600"
+          ></input>
+        </div>
+        <button
+          type="submit"
+          className="mb-6 w-full bg-blue-600 text-white font-medium text-sm rounded shadow-md hover:bg-blue-700 hover:shadow-lg px-7 py-3 uppercase focus:bg-blue-700 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+        >
+          {" "}
+          Create Listing
+        </button>
       </form>
     </main>
   );
