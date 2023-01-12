a2e21ada7a4c438f5c56d047e37a4689ebbb1ff8
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2023-01-12 23:48:45 +0530

    geolocation added and image upload feature got an error.

--- realtor_clone/src/pages/CreateListing.jsx
+++ realtor_clone/src/pages/CreateListing.jsx
@@ -2,9 +2,18 @@ import React from "react";
 import { useState } from "react";
 import Spinner from "../components/Spinner";
 import { toast } from "react-toastify";
+import { getAuth } from "firebase/auth";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { v4 as uuidv4 } from "uuid";
 
 export default function CreateListing() {
-  const [geolocationEnabled, setGeolocationEnabled] = useState(true);
+  const auth = getAuth();
+  const [geolocationEnabled, setGeolocationEnabled] = useState(false);
   const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
     type: "rent",
@@ -38,6 +47,9 @@ export default function CreateListing() {
     longitude,
     images,
   } = formData;
+
+  let geolocation = {};
+  let location;
   function onChange(e) {
     let boolean = null;
     if (e.target.value === "false") {
@@ -51,7 +63,7 @@ export default function CreateListing() {
     if (e.target.files) {
       setFormData((prevState) => ({
         ...prevState,
-        images: e.target.value,
+        images: e.target.files,
       }));
     }
     // For Text / Booleans / Numbers
@@ -63,7 +75,7 @@ export default function CreateListing() {
       }));
     }
   }
-  function onSubmit(e) {
+  async function onSubmit(e) {
     e.preventDefault();
     setLoading(true);
     if (discountedPrice >= regularPrice) {
@@ -77,6 +89,59 @@ export default function CreateListing() {
       toast.error("Maximum 6 images are allowed");
       return;
     }
+
+    if (!geolocationEnabled) {
+      geolocation.lat = latitude;
+      geolocation.lng = longitude;
+    }
+
+    async function storeImage(image) {
+      return new Promise((resolve, reject) => {
+        const storage = getStorage();
+        const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+        const storageRef = ref(storage, fileName);
+
+        const uploadTask = uploadBytesResumable(storageRef, image);
+
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            // Observe state change events such as progress, pause, and resume
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is " + progress + "% done");
+            switch (snapshot.state) {
+              case "paused":
+                console.log("Upload is paused");
+                break;
+              case "running":
+                console.log("Upload is running");
+                break;
+            }
+          },
+          (error) => {
+            // Handle unsuccessful uploads
+            reject();
+          },
+          () => {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              resolve(downloadURL);
+            });
+          }
+        );
+      });
+    }
+
+    const imgUrls = await Promise.all(
+      [...images].map((image) => storeImage())
+    ).catch((error) => {
+      setLoading(false);
+      toast.error("Could'nt upload image");
+    });
+    console.log(imgUrls);
   }
 
   if (loading) {
